c9aab079602d95bc0325fedf8265746d9a0f727c
Neshma Patel <neshmapatel1399@gmail.com>
2020-06-05 18:39:00 +0530

    vcr gem used for testing when there is http request

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 require 'rails_helper'
 RSpec.describe Api::V1::UsersController  do
-    describe 'index' do
+    
+    describe "GET #index" do
         before do
            get :index
         end
@@ -12,15 +13,6 @@ RSpec.describe Api::V1::UsersController  do
        
     end
 
-    # to create user
-
-    # describe "POST #create" do
-    #     it "creates user" do 
-    #         user_params = Factory.attributes_for(:user)
-    #         expect { post :create, :user => user_params }.to change(User, :count).by(1) 
-    #       end
-    # end 
-
     describe 'create' do
         it 'successfully creates a new user' do
           expect{
@@ -28,30 +20,18 @@ RSpec.describe Api::V1::UsersController  do
             }.to change(User,:count).by(1)
         end
     end
-    # describe 'PATCH #update' do
-    #      it "updates the user" do
-    #           patch :update, id: @user.id, user: { username: "xyz", password_digest: "xyz"}
-    #           expect(response).to be_redirect
-    #     end
-    # end
-    # describe "update" do
-    #     # before do
-    #     #    patch :update
-    #     # end
+  
+    describe "update" do
+        it "successfully updates the user" do
+            expect(response).to have_http_status(:success)  
+        end       
+    end
     
-    #     it "successfully updates the user" do
-    #         expect(response).to have_http_status(:success)  
-    #     end
-    #     # it "unable to update user" do
-    #     #     expect(response).to have_http_status(:)
-    #     # end
-       
-    # end
-    
-    # describe "destroy" do
-    #     it "user destroyed successfully" do
-    #         expect(response).to have_http_status(:success)
-    #     end
-    # end
+    describe "destroy" do
+        it "user destroyed successfully" do
+            expect(response).to have_http_status(:success)
+        end
+    end
     
 end
+
